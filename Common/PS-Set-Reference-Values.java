c00f4eccd6d9d2deb448afa1ab6c1ed07591132c
peufracio-eqx <peufracio@ap.equinix.com>
2024-04-09 10:21:58 +0800

    added system name array list handler

--- Common/PS-Set-Reference-Values.java
+++ Common/PS-Set-Reference-Values.java
@@ -10,20 +10,17 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
     Properties props = dataContext.getProperties(i);
 
     String dataText = is.text;
-    //logger.info("dataText1-->"+is.text);
     //String newDataText = ((((dataText.replace("&","&amp;")).replace("\"","&quot;")).replace("\'","&apos;")).replace("<","&lt;")).replace(">","&gt;");
-    //logger.info("newDataText-->"+newDataText);
 
     def jsonObject = new JsonSlurper().parse(dataContext.getStream(i));
     def bizRefvalue = "", SourceID = "", AccountName = "", SystemName = "", errCode = "", errMsg = "";
     def validMsg = "N";
 
     try {
-
         if(jsonObject.containsKey("Task")){
             SourceID = jsonObject.Task.Body.eqxSourceId;
             AccountName = jsonObject.Task.Body.partyAccountName;
-            SystemName = jsonObject.Task.Body.externalSystemName;
+            SystemName =    jsonObject.Task.Body.externalSystemName;
         }else if(jsonObject.containsKey("task")){
             SourceID = jsonObject.task.body.eqxSourceId;
             AccountName = jsonObject.task.body.partyAccountName;
@@ -32,9 +29,7 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
             SourceID = jsonObject.body.eqxSourceId;
             AccountName = jsonObject.body.partyAccountName;
             SystemName = jsonObject.body.externalSystemName;
-                if(SourceID){
-                    validMsg = "Y";
-                }
+                if(SourceID){validMsg = "Y";}
         }else{
             SourceID = "ERR-UNKNOWN";
             AccountName = "ERR-UNKNOWN";
@@ -44,6 +39,11 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
             errCode = "400-Bad Request";
             errMsg = "Bad Request:\n" + jsonObject;
         }
+        if(SystemName instanceof ArrayList){
+            SystemName = SystemName.get(0);
+        }else{
+            SystemName = SystemName.toString();
+        }   
         bizRefvalue = SourceID + "|" + AccountName + "|" + SystemName;
     }
     catch(Exception ex) {
@@ -60,8 +60,6 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
         ExecutionUtil.setDynamicProcessProperty("DPP_ErrorCode", errCode, false);
         ExecutionUtil.setDynamicProcessProperty("DPP_ErrorMessage", errMsg, false);        
 
-        //ExecutionUtil.setDynamicProcessProperty("DPP_BizRefName", "SourceID|AccountName|SystemName", false);
-        //ExecutionUtil.setDynamicProcessProperty("DPP_BizRefName1", "SourceID|AccountName|SystemName", false);
         ExecutionUtil.setDynamicProcessProperty("DPP_BizRefValue", bizRefvalue, false);
         ExecutionUtil.setDynamicProcessProperty("DPP_TransactionID", bizRefvalue, false);
         ExecutionUtil.setDynamicProcessProperty("DPP_3001_ValidMsg", validMsg, false);
